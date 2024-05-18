a13b3355cd1a2ec1373807b573921e8128cfd841
Ethan Diedericks <diedericksethan@gmail.com>
2024-05-18 06:46:17 +0200

    djoser and email configuration

--- core/settings.py
+++ core/settings.py
@@ -1,16 +1,19 @@
 from dotenv import load_dotenv
 from pathlib import Path
 from django.core.management.utils import get_random_secret_key
-import os
+from os import getenv, path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-load_dotenv()
+dotenv_file = BASE_DIR / ".env.dev"
+
+if path.isfile(dotenv_file):
+    load_dotenv(dotenv_file)
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
+SECRET_KEY = getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DJANGO_DEBUG", "False")
+DEBUG = getenv("DJANGO_DEBUG", "False")
 
 ALLOWED_HOSTS = []
 
@@ -28,7 +31,10 @@ INSTALLED_APPS = [
     "accounts.apps.AccountsConfig",
     "listing.apps.ListingConfig",
     # third-party apps
+    "corsheaders",
     "rest_framework",
+    "djoser",
+    "sendgrid",
 ]
 
 AUTH_USER_MODEL = "accounts.MarketplaceUser"
@@ -36,6 +42,7 @@ AUTH_USER_MODEL = "accounts.MarketplaceUser"
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -74,7 +81,14 @@ DATABASES = {
     }
 }
 
-
+# Email settings
+SENDGRID_API_KEY = getenv("SENDGRID_API_KEY")
+DEFAULT_FROM_EMAIL = getenv("DEFAULT_FROM_EMAIL")
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_HOST_USER = "apikey"
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -121,8 +135,43 @@ MEDIA_ROOT = BASE_DIR / "media"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
+AUTHENTICATION_BACKENDS = [
+    "django.contrib.auth.backends.ModelBackend",
+]
+
 REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "accounts.authentication.CustomJWTAuthentication",
+    ],
     "DEFAULT_PERMISSION_CLASSES": [
-        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
-    ]
+        "rest_framework.permissions.IsAuthenticated",
+    ],
 }
+
+DJOSER = {
+    "PASSWORD_RESET_CONFIRM_URL": "password-reset/{uid}/{token}",
+    "ACTIVATION_URL": "activation/{uid}/{token}",
+    "SEND_ACTIVATION_EMAIL": True,
+    "USER_CREATE_PASSWORD_RETYPE": True,
+    "PASSWORD_RESET_CONFIRM_RETYPE": True,
+    "TOKEN_MODEL": None,
+}
+
+
+AUTH_COOKIE = "access"
+AUTH_COOKIE_MAX_AGE = 60 * 60 * 24 * 30
+AUTH_COOKIE_SECURE = getenv("AUTH_COOKIE_SECURE", "True") == "True"
+AUTH_COOKIE_HTTP_ONLY = True
+AUTH_COOKIE_PATH = "/"
+AUTH_COOKIE_SAMESITE = "None"
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+SITE_NAME = getenv("SITE_NAME")
+DOMAIN = getenv("DOMAIN")
+
+CORS_ALLOWED_ORIGINS = getenv(
+    "CORS_ALLOWED_ORIGINS", "http://localhost:3000,http://127.0.0.1:3000"
+).split(",")
+
+CORS_ALLOW_CREDENTIALS = True
