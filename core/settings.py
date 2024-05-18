273065654096c4c1c73ccadce6aa83c49627ecec
Ethan Diedericks <diedericksethan@gmail.com>
2024-05-18 02:26:01 +0200

    best practices installed app names

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # local apps
-    "accounts",
+    "accounts.apps.AccountsConfig",
     # third-party apps
     "rest_framework",
 ]
