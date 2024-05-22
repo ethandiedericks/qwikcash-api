35577c4f6e7ff24037337d04683aa443e7d1cab5
Ethan Diedericks <diedericksethan@gmail.com>
2024-05-22 19:05:52 +0200

    views for item, vehicle and property models created

--- listing/views.py
+++ listing/views.py
@@ -1,3 +1,78 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.response import Response
+from rest_framework import status, filters
+from .models import ItemListing, VehicleListing, PropertyListing, Photo
+from .serializers import (
+    ItemListingSerializer,
+    VehicleListingSerializer,
+    PropertyListingSerializer,
+)
+from django.db.models import Q
 
-# Create your views here.
+
+class ItemListingAPIView(ListCreateAPIView):
+    queryset = ItemListing.objects.select_related("location").prefetch_related("photos")
+    serializer_class = ItemListingSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = [
+        "title",
+        "description",
+        "category",
+        "location__city",
+        "location__state",
+    ]
+    ordering_fields = ["price", "created_at", "views", "favorites"]
+
+
+class VehicleListingAPIView(ListCreateAPIView):
+    queryset = VehicleListing.objects.select_related("location").prefetch_related(
+        "photos"
+    )
+    serializer_class = VehicleListingSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = [
+        "title",
+        "description",
+        "make",
+        "model",
+        "type",
+        "location__city",
+        "location__state",
+    ]
+    ordering_fields = ["price", "created_at", "views", "favorites"]
+
+
+class PropertyListingAPIView(ListCreateAPIView):
+    queryset = PropertyListing.objects.select_related("location").prefetch_related(
+        "photos"
+    )
+    serializer_class = PropertyListingSerializer
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = [
+        "title",
+        "description",
+        "type",
+        "location__city",
+        "location__state",
+    ]
+    ordering_fields = ["price", "created_at", "views", "favorites"]
+
+
+class ItemListingDetailAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = ItemListing.objects.select_related("location").prefetch_related("photos")
+    serializer_class = ItemListingSerializer
+
+
+class VehicleListingDetailAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = VehicleListing.objects.select_related("location").prefetch_related(
+        "photos"
+    )
+    serializer_class = VehicleListingSerializer
+
+
+class PropertyListingDetailAPIView(RetrieveUpdateDestroyAPIView):
+    queryset = PropertyListing.objects.select_related("location").prefetch_related(
+        "photos"
+    )
+    serializer_class = PropertyListingSerializer
