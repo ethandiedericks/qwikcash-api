37a97550210c86270e938cdf7aadf552280a358b
Ethan Diedericks <diedericksethan@gmail.com>
2024-05-26 02:58:07 +0200

    added urls for getting listings without auth and item listings crud urls

--- listing/urls.py
+++ listing/urls.py
@@ -1,20 +1,16 @@
 from django.urls import path
 from .views import (
-    ItemListingView,
-    ItemListingCreateView,
-    VehicleListingView,
-    VehicleListingCreateView,
-    PropertyListingView,
-    PropertyListingCreateView,
+    ItemListingListView,
+    ItemListingListCreateAPIView,
+    ItemListingDetailAPIView,
+    VehicleListingListView,
+    PropertyListingListView,
 )
 
 urlpatterns = [
-    path("item/", ItemListingView.as_view(), name="item-list"),
-    path("item/create/", ItemListingCreateView.as_view(), name="item-create"),
-    path("vehicle/", VehicleListingView.as_view(), name="vehicle-list"),
-    path("vehicle/create/", VehicleListingCreateView.as_view(), name="vehicle-create"),
-    path("property/", PropertyListingView.as_view(), name="property-list"),
-    path(
-        "property/create/", PropertyListingCreateView.as_view(), name="property-create"
-    ),
+    path("items/", ItemListingListView.as_view(), name="item-list"),
+    path("items/create/", ItemListingListCreateAPIView.as_view(), name="item-create"),
+    path("items/<int:pk>/", ItemListingDetailAPIView.as_view(), name="item-detail"),
+    path("vehicles/", VehicleListingListView.as_view(), name="vehicle-list"),
+    path("properties/", PropertyListingListView.as_view(), name="property-list"),
 ]
