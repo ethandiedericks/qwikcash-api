a0bfecef53cd382661347b8fc3f870552ddcd6ef
Ethan Diedericks <diedericksethan@gmail.com>
2024-05-26 02:56:29 +0200

    added views for getting listings without being signed in and all item lisiting views

--- listing/views.py
+++ listing/views.py
@@ -1,6 +1,8 @@
 from rest_framework.views import APIView
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
+from rest_framework import status
 from .models import ItemListing, VehicleListing, PropertyListing, Photo
 from .serializers import (
     ItemListingSerializer,
@@ -9,7 +11,8 @@ from .serializers import (
 )
 
 
-class ItemListingView(APIView):
+class ItemListingListView(APIView):
+    permission_classes = [AllowAny]
 
     def get(self, request):
         item_listings = ItemListing.objects.select_related("location").prefetch_related(
@@ -19,18 +22,8 @@ class ItemListingView(APIView):
         return Response(serializer.data)
 
 
-class ItemListingCreateView(APIView):
-    authentication_classes = [JWTAuthentication]
-
-    def post(self, request):
-        serializer = ItemListingSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(user=request.user)
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
-
-
-class VehicleListingView(APIView):
+class VehicleListingListView(APIView):
+    permission_classes = [AllowAny]
 
     def get(self, request):
         vehicle_listings = VehicleListing.objects.select_related(
@@ -40,18 +33,8 @@ class VehicleListingView(APIView):
         return Response(serializer.data)
 
 
-class VehicleListingCreateView(APIView):
-    authentication_classes = [JWTAuthentication]
-
-    def post(self, request):
-        serializer = VehicleListingSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save(user=request.user)
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
-
-
-class PropertyListingView(APIView):
+class PropertyListingListView(APIView):
+    permission_classes = [AllowAny]
 
     def get(self, request):
         property_listings = PropertyListing.objects.select_related(
@@ -61,12 +44,83 @@ class PropertyListingView(APIView):
         return Response(serializer.data)
 
 
-class PropertyListingCreateView(APIView):
+class ItemListingListCreateAPIView(APIView):
     authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        item_listings = ItemListing.objects.select_related("location").prefetch_related(
+            "photos"
+        )
+        serializer = ItemListingSerializer(item_listings, many=True)
+        return Response(serializer.data)
 
     def post(self, request):
-        serializer = PropertyListingSerializer(data=request.data)
+        serializer = ItemListingSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save(user=request.user)
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ItemListingDetailAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self, pk):
+        try:
+            return ItemListing.objects.get(pk=pk)
+        except ItemListing.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+    def get(self, request, pk):
+        item_listing = self.get_object(pk)
+
+        if not item_listing:
+            return Response(
+                {"message": "Item does not exist"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        serializer = ItemListingSerializer(item_listing)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        item_listing = self.get_object(pk)
+
+        if not item_listing:
+            return Response(
+                {"message": "Item does not exist"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        serializer = ItemListingSerializer(item_listing, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def patch(self, request, pk):
+        item_listing = self.get_object(pk)
+
+        if not item_listing:
+            return Response(
+                {"message": "Item does not exist"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        serializer = ItemListingSerializer(
+            item_listing, data=request.data, partial=True
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        item_listing = self.get_object(pk)
+
+        if not item_listing:
+            return Response(
+                {"message": "Item does not exist"}, status=status.HTTP_404_NOT_FOUND
+            )
+
+        item_listing.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
